96cb1bcb04af55709856b14d47cc5e92b24a8fe5
wueuwoeoe <122522450+wueuwoeoe@users.noreply.github.com>
2023-01-12 13:31:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,16 +61,16 @@ global.socialm = "GitHub: Elgazar" //ur github or insta name
 global.location = "Egypt, Cairo, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['201098906252']
-global.ownertag = '201098906252' //ur tag number
-global.botname = '𝑩𝑶𝑻 𝑬𝑳𝑮𝑨𝒁𝑨𝑹🐼🍷 ٫' //ur bot name
+global.owner = ['967734404224']
+global.ownertag = '967734404224' //ur tag number
+global.botname = '𝑩𝑶𝑻 𝐌𝐈𝐊𝐎𝐓𝐎🐼🍷 ٫' //ur bot name
 global.linkz = "https://telegra.ph/file/f504bef433c8fd31d1674.jpg" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/f504bef433c8fd31d1674.jpg" //ur website to be displayed
 global.botscript = 'https://chat.whatsapp.com/Gb62eN2S5yI9mhfdn6Ma4z' //script link
 global.reactmoji = "🌚" //ur menu react emoji
 global.themeemoji = "🌞" //ur theme emoji
 global.packname = "⍣ الجزار الوزير يروحي ⍣" //ur sticker watermark packname
-global.author = "⍣ 01098906252 ⍣" //ur sticker watermark author
+global.author = "⍣ 967733613094 ⍣" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['201098906252'] //ur premium numbers
+global.premium = ['967734404224'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
